8b133c1d55d5fbfa0dc8efb2f365bf14e5427204
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-05 00:40:36 -0500

    Implement player rotation towards direction of motion
    This was done through a lot of pain and tears.
    
    A new function that is designed to sync the player rotation
    to the direciton of motion was added.
    
    It takes the signed angle between the z axis of the player and the
    camera and uses that to determine the desired location.

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 @export var camera: Node3D
 @export var playerBody: Node3D
+@export var playerCollsionMesh: Node3D
+@export var playerTurnSpeed: float = 5
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 10
@@ -11,17 +13,49 @@ const JUMP_VELOCITY = 10
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
-	_handle_input_logic(delta)
+	print(transform)
+	var translational_input= Input.get_vector("player_strafeleft", "player_straferight", "player_forward", "player_backward")
+
+	_handle_input_logic(translational_input, delta)
+	_player_view_rotation_sync(translational_input, delta)
 
 	move_and_slide()
 
-func _handle_input_logic(delta): 
-	_handle_jump_logic(delta)
-	_handle_translational_motion_logic(delta)
+func _player_view_rotation_sync(translational_input, delta):
+	var direction_basis = (camera.transform.basis * Vector3(translational_input.x, 0, translational_input.y)).normalized()
+	var player_body_axis = playerBody.transform.basis.z
+	# var direction_basis = camera.transform.basis.x
 
-func _handle_translational_motion_logic(delta):
-	var input_dir = Input.get_vector("player_strafeleft", "player_straferight", "player_forward", "player_backward")
-	var direction_basis = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# Relative to base node (currently the CharacterBody3D)
+
+	var angular_difference = player_body_axis.signed_angle_to(direction_basis, transform.basis.y)
+	var delta_rotation = lerp_angle(0, angular_difference, delta * playerTurnSpeed)
+	if _translational_input_pressed():
+		# This assumes that the y axis of the player and the camera are the same
+		# Find the angle between the player and the camera
+		# Use angular interpolation to figure out the set angle
+
+		
+		playerBody.rotate_object_local(Vector3.UP, delta_rotation)
+		playerCollsionMesh.rotate_object_local(Vector3.UP, delta_rotation)
+
+
+func _translational_input_pressed():
+	return Input.is_action_pressed("player_forward")\
+	or Input.is_action_pressed("player_backward")\
+	or Input.is_action_pressed("player_strafeleft")\
+	or Input.is_action_pressed("player_straferight")
+
+	
+
+	
+
+func _handle_input_logic(translational_input, delta): 
+	_handle_jump_logic(delta)
+	_handle_translational_motion_logic(translational_input, delta)
+
+func _handle_translational_motion_logic(translational_input, delta):
+	var direction_basis = (camera.transform.basis * Vector3(translational_input.x, 0, translational_input.y)).normalized()
 	if direction_basis:
 		velocity.x = direction_basis.x * SPEED
 		velocity.z = direction_basis.z * SPEED

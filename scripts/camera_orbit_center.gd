0882ab43dec54f3510c4f19f1b2ca80c6793ec4f
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-04 20:46:30 -0500

    Implement camera orbiting by giving it a hinge
    A Node3D called camera orbit center is used as the parent.
    The node itself is rotated and the camera follows thus simulating
    an "orbiting" motion.
    
    (It is the alternative using some linear algebra to simulate
    the calculation of offset rotations.)

---
+++ scripts/camera_orbit_center.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@export var mouse_rotation_sensitivity:float = .1 #Degrees per pixel
+@export var camera: Camera3D 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		#Both are in degrees set to 1 degree of rotation per pixel
+		var camera_rotation = event.relative * mouse_rotation_sensitivity * deg_to_rad(1)
+
+		camera_rotation.x *= -1 #Negative because clockwise (righthand rule) is the positive direction
+
+		print(camera_rotation)
+
+		# Handle orbit motion 
+		rotate_object_local(Vector3.UP, camera_rotation.x) 

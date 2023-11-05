4479a1f6c31faba88dbe913d21cd55ea8b9ce0e2
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-04 21:54:43 -0500

    Add new input map for player
    Update script again from mangled rebase

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 class_name Player
 extends CharacterBody3D
 
+@export var camera: Node3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 10
@@ -20,8 +21,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("player_strafeleft", "player_straferight", "player_forward", "player_backward")
+	var direction = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED

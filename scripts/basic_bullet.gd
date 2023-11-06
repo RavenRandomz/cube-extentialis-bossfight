c7dd80fde052a31767441198495284b077544f7d
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-05 23:39:53 -0600

    Implement bullet damage

--- scripts/basic_bullet.gd
+++ scripts/basic_bullet.gd
@@ -1,15 +1,17 @@
 extends RigidBody3D
 
-@export var damage: float = 10
+@export var damage: float = 100
 @export var speed: float = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	linear_velocity = transform.basis.z * speed
-	pass # Replace with function body.
+	# Initialize collision detect
+	contact_monitor = true
+	max_contacts_reported = 1
 
+	linear_velocity = transform.basis.x * speed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_body_entered(body:Node):
+	if body.has_method("damage"):
+		body.damage(damage)
+	queue_free()

5a886f93e7920177713ff2ba45d9586dfd239645
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-07 17:06:25 -0600

    Create accessor functions to player and clean up styling

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,47 +1,41 @@
 class_name Player
 extends CharacterBodyEntity3D
 
-@export var health: float = 1000
-@export var camera: Node3D
-@export var playerBody: Node3D
-@export var playerCollsionMesh: Node3D
-@export var playerTurnSpeed: float = 5
-@export var display: Player_HUD
+@export var camera:Node3D
+@export var display:Player_HUD
+@export var bullet_source:BasicBulletSource 
+@export var input_handler:PlayerInputHandler
+@export var body_handler:PlayerBodyHandler
 
-@export var min_health: float = 0
-@export var max_health: float = 1000
-@export var bullet_source: BasicBulletSource 
-@export var input_handler: PlayerInputHandler
+var movement_handler = PlayerMovementHandler.new(self)
 
-var player_movement_handler = PlayerMovementHandler.new(self)
-
-
-var basic_bullet = preload("res://projectile/basic_bullet.tscn")
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @export var speed:float = 5.0:
 	get:
-		return player_movement_handler.speed
+		return movement_handler.speed
 	set(new_speed): 
-		player_movement_handler.speed = new_speed
+		movement_handler.speed = new_speed
 
 @export var jump_speed:float = 10.0:
 	get:
-		return player_movement_handler.jump_speed
+		return movement_handler.jump_speed
 	set(new_jump_speed):
-		player_movement_handler.jump_speed = new_jump_speed
+		movement_handler.jump_speed = new_jump_speed
 
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@export var body_turn_speed:float = 10.0:
+	get:
+		return body_handler.body_turn_speed
+	set(new_body_turn_speed):
+		body_handler.body_turn_speed = new_body_turn_speed
 
 func _ready():
 	super._ready()
 	display.set_health_bar_range(min_health, max_health)
 	display.update_health_bar(health)
-	add_child(player_movement_handler)
+	add_child(movement_handler)
 
-func _physics_process(delta):
-	# _player_view_rotation_sync(get_desired_movement_direction(), delta)
+func _physics_process(_delta):
 	move_and_slide()
 
 func _on_player_bullet_fire():
@@ -50,12 +44,12 @@ func _on_player_bullet_fire():
 
 func _on_player_jump_input():
 	if (not is_stunned()):
-		player_movement_handler.jump()
+		movement_handler.jump()
 
 func _on_translational_motion_input(translational_input_map):
 	if (not is_stunned()):
 		var direction = (camera.transform.basis * Vector3(translational_input_map.x, 0, translational_input_map.y)).normalized()
-		player_movement_handler.controlled_translational_motion(direction)
+		movement_handler.controlled_translational_motion(direction)
 
 func get_desired_translational_movement_direction():
 	var raw_translational_map = input_handler.get_translational_input_map()

4570d6f6a430aedd849bb7325df9151c393d9621
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-07 09:36:56 -0600

    Add basicbullet timeout to prevent scene clogging

--- scripts/basic_bullet.gd
+++ scripts/basic_bullet.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 
 @export var damage: float = 100
 @export var speed: float = 10
+@export var timeout:float = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +11,8 @@ func _ready():
 	contact_monitor = true
 	max_contacts_reported = 1
 	linear_velocity = transform.basis.z * speed
-
-func fire():
-	linear_velocity = transform.basis.z * speed
+	await get_tree().create_timer(timeout).timeout
+	queue_free()
 
 func _on_body_entered(body:Node):
 	if body.has_method("damage"):

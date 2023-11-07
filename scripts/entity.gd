e1de61ecfc230db716275eb4369a6aab004eee23
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-07 12:15:50 -0600

    Add explanation for using composition to create multiple redunant entity classes

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -1,7 +1,30 @@
 class_name Entity
 
+# It is possible to simply define Entity as an extension of Node
+# However, that would mean that the Entity would have a "Body" (in this case
+# being the CharacterBody3D. Scene tree wise, this would lead to a static
+# location (probably at 0,0,0) while the "body" moves around
+# Currently, this is deemed to be too cumbersome so because of the small
+# amount of PhysicsBody3D classes that all entities are based off of
+# For the time being, multiple inheritance is being simulated by adapters
+# So the player class and any enemy can inherit the basic funcitonalities of
+# Health and being stunned (reducing the need for boilerplate code).
+# Perhaps later, all of these redundant classes will be changed at the cost of
+# Sacrificing compatability with Godot's framework of nodes
+# (A parent Entity that inherits Node3D would mean that the player body is in the Node3D)
+# That is on the scene tree. So if the player has to be moved around, the Node3D parent would have to be changed.
+# Or that node would ismply be "disembodied" while the body is transferred to moving platforms and whatnot.
+
+
+
+@export var max_health = 1000
+@export var min_health = 0
 var health = 1000
 var stunned = false
+
+func _init(starting_health:float = 1000):
+	health = starting_health
+	
 
 func get_health() -> float:
 	return health

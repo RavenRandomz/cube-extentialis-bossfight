7fb9199522f332d1de8f64eeed61d1fffe2ba58a
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-07 13:50:10 -0600

    Create Player Input Handler
    The Input Handler is responsible for giving out signals that
    signify an input event. As well as supplying relevant input information.
    
    Because it is signal heavy, it will be designed to be placed as an
    explicit node for the Player. This design choice matches Godot Node
    design philosophy because a similar thing is done with PhysicsBodies and
    CollisionShapes.  (A child node that serves to encapsulate a specific function of the parent node

---
+++ scripts/player_input_handler.gd
@@ -0,0 +1,33 @@
+class_name PlayerInputHandler
+extends Node
+
+signal on_translational_motion_input(translational_input_map:Vector3)
+signal on_player_jump_input
+signal on_player_bullet_fire
+
+func _process(_delta):
+	_report_inputs()
+
+func _report_inputs(): 
+	if _translational_input_active():
+		var translational_input= Input.get_vector("player_strafeleft", "player_straferight", "player_forward", "player_backward")
+		on_translational_motion_input.emit(translational_input)
+
+	if _jump_input_active():
+		on_player_jump_input.emit()
+	
+	if _fire_bullet_input_active():
+		on_player_bullet_fire.emit()
+
+
+func _translational_input_active():
+	return Input.is_action_pressed("player_forward")\
+	or Input.is_action_pressed("player_backward")\
+	or Input.is_action_pressed("player_strafeleft")\
+	or Input.is_action_pressed("player_straferight")
+
+func _jump_input_active():
+	return Input.is_action_just_pressed("player_jump")
+
+func _fire_bullet_input_active():
+	return Input.is_action_pressed("player_fire_bullet")

8386ef9c5471b6ba316a080a509f5c7a6e76d037
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-07 17:10:56 -0600

    clean up basicBullet script

--- scripts/basic_bullet.gd
+++ scripts/basic_bullet.gd
@@ -5,9 +5,7 @@ extends RigidBody3D
 @export var speed: float = 10
 @export var timeout:float = 5
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# Initialize collision detect
 	contact_monitor = true
 	max_contacts_reported = 1
 	linear_velocity = transform.basis.z * speed
@@ -20,4 +18,3 @@ func _on_body_entered(body:Node):
 	
 	if not(body is BasicBullet):
 		queue_free()
-	

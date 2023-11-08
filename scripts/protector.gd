24fbd46df36c2b9a86cb26008fd5618faa189c9f
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-07 19:24:55 -0600

    Implement basic chasing functionality on Protector

--- scripts/protector.gd
+++ scripts/protector.gd
@@ -1,5 +1,9 @@
 class_name Protector extends RigidBodyEntity3D
 
+@export var _chasing_timeout:float = 5
+var _chasing = false
+var _target: Node3D
+var _chasing_force: float = 40
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,9 +11,36 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	if (not _target == null):
+		var force = _get_direction_to_target() * _chasing_force
+		apply_central_force(force)
 
+
+func _get_direction_to_target():
+	var positional_difference_vector =  _target.global_position - global_position
+	var direction = positional_difference_vector.normalized()
+	return direction
 
 func _on_bullet_detected(bullet:Node3D):
-	pass
+	if (not _chasing):
+		_target = bullet
+		_chasing = true
+		await get_tree().create_timer(_chasing_timeout).timeout
+		_stop_chase()
+
+func _stop_chase():
+		_chasing = false
+		_target = null
+		_stop_motion()
+
+func _stop_motion():
+	linear_velocity = Vector3.ZERO
+	angular_velocity = Vector3.ZERO
+
+func _on_body_entered(body):
+	if body.is_in_group("bullet"):
+		body.queue_free()
+		_stop_chase()
+
+

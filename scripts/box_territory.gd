a6fd8bfe8c0d7383b19a386c21b4cfa9eff4b6c4
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-12 18:23:27 -0600

    Add box territory
    The territory is where the Protector will roam. It is also a general
    class that can provide a random point as well as tell a subscriber that
    it has exited or has entered the territory.

---
+++ scripts/box_territory.gd
@@ -0,0 +1,23 @@
+class_name BoxTerritory extends Area3D
+
+signal watched_exited
+signal watched_entered
+
+var watch_list = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func add_to_watchlist(watchee):
+	watch_list.push(watchee)
+
+func remove_from_watchlist(member):
+	var member_index = watch_list.find(member)
+	# -1 is the value that Array.find returns if there is no match
+	if (member_index != -1):
+		watch_list.remove_at(member_index)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

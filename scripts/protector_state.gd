52a73d48076f8045939f548d372e48cfc7785926
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-13 03:54:20 -0600

    Add default methods to protector_state

--- scripts/protector_state.gd
+++ scripts/protector_state.gd
@@ -5,8 +5,17 @@ var _protector: Protector
 func _init(prot:Protector):
 	_protector = prot
 
-func _integrate_forces(state):
+func _on_ready():
 	pass
 
-func _process(delta):
+func _physics_process(_delta):
+	pass
+
+func _integrate_forces(_state):
+	pass
+
+func _process(_delta):
+	pass
+
+func _on_bullet_captured(_bullet:Node3D):
 	pass

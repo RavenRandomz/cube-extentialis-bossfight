f4240aa90645d04d47c8578bd2b495ff40da868f
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-13 03:54:33 -0600

    Move chasing functionality in Protector to chase state

--- scripts/protector.gd
+++ scripts/protector.gd
@@ -13,6 +13,7 @@ var _roam_force:float = 0.5:
 
 # States
 @onready var _state_roam = ProtectorStateRoam.new(self)
+@onready var _state_chase = ProtectorStateChase.new(self)
 @onready var _state:ProtectorState = _state_roam
 
 enum State {
@@ -30,7 +31,7 @@ func set_state(state:State):
 		State.ROAM:
 			_state = _state_roam
 		State.CHASE:
-			pass
+			_state = _state_chase
 		State.COME_BACK:
 			pass
 	_state._on_ready()
@@ -39,8 +40,6 @@ func set_state(state:State):
 func _ready():
 	set_state(State.ROAM)
 
-	pass # Replace with function body.
-
 # Expects global coordinates
 func is_near(point:Vector3, distance:float):
 	return point.distance_to(global_position) < distance
@@ -48,42 +47,17 @@ func is_near(point:Vector3, distance:float):
 func get_territory():
 	return _territory
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	_state._physics_process(delta)
 
-func _old_physics_process(_delta):
-	if (not _target == null):
-		var force = _get_direction_to_target() * _chasing_force
-		apply_central_force(force)
-	else:
-		_stop_motion()
-
-
-func _get_direction_to_target():
-	var positional_difference_vector =  _target.global_position - global_position
-	var direction = positional_difference_vector.normalized()
-	return direction
-
 func _on_bullet_detected(bullet:Node3D):
-	if (not _chasing):
-		_target = bullet
-		_chasing = true
-		await get_tree().create_timer(_chasing_timeout).timeout
-		_stop_chase()
-
-func _stop_chase():
-		_chasing = false
-		_target = null
-		_stop_motion()
-
-func _stop_motion():
-	linear_velocity = Vector3.ZERO
-	angular_velocity = Vector3.ZERO
+	set_state(State.CHASE)
+	_target = bullet
 
 func _on_body_entered(body):
 	if body.is_in_group("bullet"):
-		body.queue_free()
-		_stop_chase()
+		_state._on_bullet_captured(body)
+		_target = null
+		
 
 
--- scripts/protector_state_chase.gd
+++ scripts/protector_state_chase.gd
@@ -1,11 +1,20 @@
-extends ProtectorState
+class_name ProtectorStateChase extends ProtectorState
 
+var _target:Node3D
+var _chasing_force:float = 40
+var _captured_cooldown:float = 5
+var _chasing_timeout:float = 10
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	await get_tree().create_timer(_chasing_timeout).timeout
+	_protector.set_state(Protector.State.ROAM)
 
+func _integrate_forces(state):
+	if (not _protector._target == null):
+		var force = _protector.global_position.direction_to(_protector._target.global_position) * _chasing_force
+		state.apply_force(force)
+	else:
+		state.linear_velocity = Vector3.ZERO
+		await _protector.get_tree().create_timer(_captured_cooldown).timeout
+		_protector.set_state(Protector.State.ROAM)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass

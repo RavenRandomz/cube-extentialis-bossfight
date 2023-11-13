058b9d29bf152c9a40d52e46ca9b2301bb1e155b
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-12 18:43:21 -0600

    Add random position generation to box_territory

--- scripts/box_territory.gd
+++ scripts/box_territory.gd
@@ -3,11 +3,22 @@ class_name BoxTerritory extends Area3D
 signal watched_exited
 signal watched_entered
 
+@onready var shape: BoxShape3D = get_node("CollisionShape3D").get_shape()
+
 var watch_list = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+# Generats a random global position within its bounds
+func generate_random_internal_position() -> Vector3:
+	var origin = global_position
+
+	var offset = Vector3(randf_range(-shape.size.x, shape.size.x), 
+						randf_range(-shape.size.y, shape.size.y),
+						randf_range(-shape.size.z, shape.size.z))
+	return origin + offset
 
 func add_to_watchlist(watchee):
 	watch_list.push(watchee)

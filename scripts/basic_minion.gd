9e9e153224e29bb35d2b6238290f95d08ff49f99
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-14 20:06:53 -0600

    Add states to basic minion

--- scripts/basic_minion.gd
+++ scripts/basic_minion.gd
@@ -1,9 +1,11 @@
 class_name BasicMinion extends CharacterBodyEntity3D
 
+@onready var _state_attack:BasicMinionStateAttack = BasicMinionStateAttack.new(self)
+@onready var _state_idle:BasicMinionStateIdle = BasicMinionStateIdle.new(self)
 
 func _on_player_entered(player:Player):
+
 	print(str(player) + "Entered")
-	pass # Replace with function body.
 
 
 func _on_player_exited(player:Player):

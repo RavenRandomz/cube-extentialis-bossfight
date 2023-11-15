f04da9e2e56f4d1870f90856e938b14d3c52d82c
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-14 20:19:16 -0600

    Add state change to basic minion

--- scripts/basic_minion.gd
+++ scripts/basic_minion.gd
@@ -8,6 +8,19 @@ enum State {
 @onready var _state_attack:BasicMinionStateAttack = BasicMinionStateAttack.new(self)
 @onready var _state_idle:BasicMinionStateIdle = BasicMinionStateIdle.new(self)
 
+@onready var _state = _state_idle
+
+var _current_state = State.IDLE
+
+func set_state(state:State):
+	match (state):
+		State.ATTACK:
+			_state = _state_attack
+			_current_state = State.ATTACK
+		State.IDLE:
+			_state = _state_idle
+			_current_state = State.IDLE	
+	_state._ready()
 
 func _on_player_entered(player:Player):
 

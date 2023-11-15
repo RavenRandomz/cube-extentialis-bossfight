99df1a29c748026c3aa94c960532cdc618ed2139
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-14 20:04:53 -0600

    rename minion state scripts to include basic

--- scripts/basic_minion.gd
+++ scripts/basic_minion.gd
@@ -1,1 +1,11 @@
 class_name BasicMinion extends CharacterBodyEntity3D
+
+
+func _on_player_entered(player:Player):
+	print(str(player) + "Entered")
+	pass # Replace with function body.
+
+
+func _on_player_exited(player:Player):
+	print(str(player) + "Exited")
+	pass # Replace with function body.

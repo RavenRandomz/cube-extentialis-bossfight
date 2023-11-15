c8b96a1909c8f79721dd7c75c507675e8fc35404
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-14 19:43:35 -0600

    Add basic minion state

---
+++ basic_minion_state.gd
@@ -0,0 +1,22 @@
+class_name  BasicMinionState extends CharacterBodyEntity3D
+
+var _basic_minion:BasicMinion
+
+func _init(basic_minion):
+	_basic_minion = basic_minion
+
+
+func _on_ready():
+	pass
+
+func _physics_process(_delta):
+	pass
+
+func _integrate_forces(_state):
+	pass
+
+func _process(_delta):
+	pass
+
+func _on_bullet_captured(_bullet:Node3D):
+	pass

7ff5d0b5e8f41f0c4b772cc5574c0f838e9780fb
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-14 20:07:37 -0600

    Add state enumerator to basic minion

--- scripts/basic_minion.gd
+++ scripts/basic_minion.gd
@@ -1,7 +1,13 @@
 class_name BasicMinion extends CharacterBodyEntity3D
 
+enum State {
+	ATTACK,
+	IDLE
+}
+
 @onready var _state_attack:BasicMinionStateAttack = BasicMinionStateAttack.new(self)
 @onready var _state_idle:BasicMinionStateIdle = BasicMinionStateIdle.new(self)
+
 
 func _on_player_entered(player:Player):
 

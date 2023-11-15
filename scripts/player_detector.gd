424b7011a2f95338a841c2b752d1193d854b363d
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-14 20:05:15 -0600

    Add basic minion player detection functionality

--- scripts/player_detector.gd
+++ scripts/player_detector.gd
@@ -1,13 +1,13 @@
 class_name PlayerDetector extends Area3D
 
-signal player_entered
-signal player_exited
+signal player_entered(player:Player)
+signal player_exited(player:Player)
 
 func _on_body_entered(body:Node3D):
 	if (body is Player):
-		player_entered.emit()
+		player_entered.emit(body)
 
 
 func _on_body_exited(body:Node3D):
 	if (body is Player):
-		player_exited.emit()
+		player_exited.emit(body)

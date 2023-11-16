79584232182ffcbe1069047518351ef6f6a4a702
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-16 12:13:05 -0600

    Have bullet source add blame to basic bullet that it fires

--- scripts/basic_bullet_source.gd
+++ scripts/basic_bullet_source.gd
@@ -3,13 +3,16 @@ extends Node3D
 
 @export var bullet_scene:PackedScene = preload("res://projectile/basic_bullet.tscn")
 @export var _cool_down:float = 0.1
+@export var _blame_node: Node3D # Who to blame for the firing of the bullet
 var _firing:bool = false
 
 
+
 func fire():
-	var bullet: RigidBody3D = bullet_scene.instantiate()
+	var bullet: BasicBullet = bullet_scene.instantiate()
 	if (not _firing):
 		_firing = true
+		bullet.set_set_source(_blame_node)
 		bullet.global_position = global_position
 		bullet.transform.basis = global_transform.basis
 		get_tree().root.add_child(bullet)

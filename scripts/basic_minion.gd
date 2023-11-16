dab0c674255bba86b356bd8ab400a253a464168b
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-16 13:14:23 -0600

    Add minion dying functionality and ability to get damaged

--- scripts/basic_minion.gd
+++ scripts/basic_minion.gd
@@ -10,9 +10,13 @@ enum State {
 
 @onready var _state = _state_idle
 @export var _gun:Gun
+
 var DEFAULT_GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var _current_state = State.IDLE
+
+func _ready():
+	super._ready()
 
 func set_state(state:State):
 	match (state):
@@ -41,3 +45,10 @@ func _gravity_pull(delta):
 	if not is_on_floor():
 		#print("Not on floor")
 		velocity.y -= DEFAULT_GRAVITY * delta
+
+func _on_death():
+	queue_free()
+
+func damage(damage_amount:float):
+	print(health)
+	super.damage(damage_amount)

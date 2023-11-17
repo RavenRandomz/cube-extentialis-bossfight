fd55195ce2641526d641d70ccef77ccea15f9a11
RavenRandomz <raven.raziel.official@gmail.com>
2023-11-16 21:33:44 -0600

    Add continous fire mode for basic bullet source

--- scripts/basic_bullet_source.gd
+++ scripts/basic_bullet_source.gd
@@ -5,12 +5,13 @@ extends Node3D
 @export var _cool_down:float = 0.1
 @export var _blame_node: Node3D # Who to blame for the firing of the bullet
 var _firing:bool = false
+var _continuous_fire:bool = false
 
 
 
 func fire():
-	var bullet: BasicBullet = bullet_scene.instantiate()
 	if (not _firing):
+		var bullet: BasicBullet = bullet_scene.instantiate()
 		_firing = true
 		bullet.set_source(_blame_node)
 		bullet.global_position = global_position
@@ -19,6 +20,18 @@ func fire():
 		await get_tree().create_timer(_cool_down).timeout
 		_firing = false
 
+func start_continuous_fire():
+	_continuous_fire = true
+
+func stop_continous_fire():
+	_continuous_fire = false
+
+func _physics_process(delta):
+	_continuous_fire_logic()
+
+func _continuous_fire_logic():
+	if (_continuous_fire):
+		fire()
 
 func set_cooldown(cool_down:float):
 	_cool_down = cool_down
